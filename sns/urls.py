b77cde4509fc2d388c9c418eace3669e6bd87fd0
KIM-JAEHYUNG <roy121@naver.com>
2019-07-31 23:02:07 +0900

    index add

--- sns/urls.py
+++ sns/urls.py
@@ -2,12 +2,15 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from .views import index
 from .views import home
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("accounts/", include("allauth.urls")),
-    path('', home, name="home"),
+    path('', index, name="index"),
+    path('home/', home, name='home'),
     path('accounts/', include('allauth.urls')),
     path('users/', include('users.urls')),
     path('posts/', include('posts.urls')),
--- sns/views.py
+++ sns/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from posts.models import Post
 from django.db.models import Count, Avg
 
+
+def index(request):
+    return render(request, 'index.html')
+
+
 def home(request):
     user = request.user
     posts = Post.objects.all().order_by('-created_at').annotate(likes_count=Count('likes'))
