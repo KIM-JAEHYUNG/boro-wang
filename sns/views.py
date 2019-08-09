1d2406afccb3f3408ef258a86fadfd1548dfe706
KIM-JAEHYUNG <roy121@naver.com>
2019-08-09 22:52:12 +0900

    css gu & five

--- sns/views.py
+++ sns/views.py
@@ -58,124 +58,124 @@ def sport(request):
 
 def gangseo(request):
     post = Post.objects.filter(areas=0)
-    return render(request, 'gangseo.html', {'post': post})
+    return render(request, 'gangseo.html', {'posts': post})
 
 
 def gangnam(request):
     post = Post.objects.filter(areas=1)
-    return render(request, 'gangnam.html', {'post': post})
+    return render(request, 'gangnam.html', {'posts': post})
 
 
 def gangdong(request):
     post = Post.objects.filter(areas=2)
-    return render(request, 'gangdong.html', {'post': post})
+    return render(request, 'gangdong.html', {'posts': post})
 
 
 def gangbuk(request):
     post = Post.objects.filter(areas=3)
-    return render(request, 'gangbuk.html', {'post': post})
+    return render(request, 'gangbuk.html', {'posts': post})
 
 
 def guro(request):
     post = Post.objects.filter(areas=4)
-    return render(request, 'guro.html', {'post': post})
+    return render(request, 'guro.html', {'posts': post})
 
 
 def geumchon(request):
     post = Post.objects.filter(areas=5)
-    return render(request, 'geumchon.html', {'post': post})
+    return render(request, 'geumchon.html', {'posts': post})
 
 
 def guanak(request):
     post = Post.objects.filter(areas=6)
-    return render(request, 'guanak.html', {'post': post})
+    return render(request, 'guanak.html', {'posts': post})
 
 
 def gwnagjin(request):
     post = Post.objects.filter(areas=7)
-    return render(request, 'gwnagjin.html', {'post': post})
+    return render(request, 'gwnagjin.html', {'posts': post})
 
 
 def nowon(request):
     post = Post.objects.filter(areas=8)
-    return render(request, 'nowon.html', {'post': post})
+    return render(request, 'nowon.html', {'posts': post})
 
 
 def dobong(request):
     post = Post.objects.filter(areas=9)
-    return render(request, 'dobong.html', {'post': post})
+    return render(request, 'dobong.html', {'posts': post})
 
 
 def dongdaemun(request):
     post = Post.objects.filter(areas=10)
-    return render(request, 'dongdaemun.html', {'post': post})
+    return render(request, 'dongdaemun.html', {'posts': post})
 
 
 def dongjak(request):
     post = Post.objects.filter(areas=11)
-    return render(request, 'dongjak.html', {'post': post})
+    return render(request, 'dongjak.html', {'posts': post})
 
 
 def mapo(request):
     post = Post.objects.filter(areas=12)
-    return render(request, 'mapo.html', {'post': post})
+    return render(request, 'mapo.html', {'posts': post})
 
 
 def seodaemoon(request):
     post = Post.objects.filter(areas=13)
-    return render(request, 'seodaemoon.html', {'post': post})
+    return render(request, 'seodaemoon.html', {'posts': post})
 
 
 def seocho(request):
     post = Post.objects.filter(areas=14)
-    return render(request, 'seocho.html', {'post': post})
+    return render(request, 'seocho.html', {'posts': post})
 
 
 def seongdong(request):
     post = Post.objects.filter(areas=15)
-    return render(request, 'seongdong.html', {'post': post})
+    return render(request, 'seongdong.html', {'posts': post})
 
 
 def songpa(request):
     post = Post.objects.filter(areas=16)
-    return render(request, 'songpa.html', {'post': post})
+    return render(request, 'songpa.html', {'posts': post})
 
 
 def seongbuk(request):
     post = Post.objects.filter(areas=17)
-    return render(request, 'seongbuk.html', {'post': post})
+    return render(request, 'seongbuk.html', {'posts': post})
 
 
 def yongsan(request):
     post = Post.objects.filter(areas=18)
-    return render(request, 'yongsan.html', {'post': post})
+    return render(request, 'yongsan.html', {'posts': post})
 
 
 def yangchon(request):
     post = Post.objects.filter(areas=19)
-    return render(request, 'yangchon.html', {'post': post})
+    return render(request, 'yangchon.html', {'posts': post})
 
 
 def eunpyeong(request):
     post = Post.objects.filter(areas=20)
-    return render(request, 'eunpyeong.html', {'post': post})
+    return render(request, 'eunpyeong.html', {'posts': post})
 
 
 def yeongdeungpo(request):
     post = Post.objects.filter(areas=21)
-    return render(request, 'yeongdeungpo.html', {'post': post})
+    return render(request, 'yeongdeungpo.html', {'posts': post})
 
 
 def jongro(request):
     post = Post.objects.filter(areas=22)
-    return render(request, 'jongro.html', {'post': post})
+    return render(request, 'jongro.html', {'posts': post})
 
 
 def jung(request):
     post = Post.objects.filter(areas=23)
-    return render(request, 'jung.html', {'post': post})
+    return render(request, 'jung.html', {'posts': post})
 
 
 def junglang(request):
     post = Post.objects.filter(areas=24)
-    return render(request, 'junglang.html', {'post': post})
+    return render(request, 'junglang.html', {'posts': post})
